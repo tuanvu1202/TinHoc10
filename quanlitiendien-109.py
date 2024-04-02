6c496fa57d9ccc19184e8e1d3c7adae941a62825
Tuấn Vũ <97029384+tuanvu1202@users.noreply.github.com>
2024-04-02 22:22:14 +0700

    Update quanlitiendien-109.py

--- quanlitiendien-109.py
+++ quanlitiendien-109.py
@@ -1,11 +1,11 @@
-array = list(map(int, input().split()))
+danhsach = list(map(int, input().split()))
 tong = 0
-for i in range(len(array)):
-    tong+=array[i]
+for i in range(len(danhsach)):
+    tong+=danhsach[i]
 
 print('Tong so tien dien cua ca nam:', tong)
 print('Tong dien trung binh theo thang:', tong//12)
 print('Cac thang co so tien dien nhieu hon tien dien trung binh theo thang:', end=' ')
-for i in range(len(array)):
-    if(array[i] > tong//12):
+for i in range(len(danhsach)):
+    if(danhsach[i] > tong//12):
         print(i+1, end=' ')
